4953ea329b38f6af7f77e69612fcab2d15dda582
Hoa Truong <2051050145hoa@ou.edu.vn>
2022-12-05 16:39:29 +0700

    QLCB

--- Source_Code/FlightManagement/__init__.py
+++ Source_Code/FlightManagement/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_babelex import Babel
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
 from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.secret_key='rtytghkjgkgur'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:10102002@localhost/flight_management_db?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+babel = Babel(app)
 
 db = SQLAlchemy(app=app)
 
-admin = Admin(app=app, name="QUAN LY CHUYEN BAY", template_mode="bootstrap4")
-
 login = LoginManager(app=app)
+
+@babel.localeselector
+def get_locale():
+    return 'vi'
