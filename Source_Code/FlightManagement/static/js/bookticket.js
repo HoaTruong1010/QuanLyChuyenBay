f835d8f7833002a94d80c1b7d518a837090f845f
Nguyễn Đặng Tuyết Nhi <87234760+Saki2502@users.noreply.github.com>
2022-12-11 22:27:57 +0700

    update booking

--- Source_Code/FlightManagement/static/js/bookticket.js
+++ Source_Code/FlightManagement/static/js/bookticket.js
@@ -1,6 +1,21 @@
-function filter_details_ticket(str, data) {
+function filter_details_ticket() {
     let h = ''
-    for (let i = 0; i < data.length; i++) {
-        if()
-    }
+    let str = document.getElementById('from').value
+    fetch("/api/booking")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach((item, index) => {
+            if(item.airlines.name.toLowerCase().includes(str)) {
+                 h += `<tr>
+                    <td id="${index}" value="${item.name}">${item.airlines.name}</td>
+                    <td id="${index}" value="${item.plane_id}">${item.plane_id}</td>
+                    <td id="${index}" value="${item.departing_at}">${item.departing_at}</td>
+                    <td id="${index}" value="${item.arriving_at}">${item.arriving_at}</td>
+                </tr>`
+            }
+        })
+
+        const address = document.getElementById('filter_flight')
+        address.innerHTML = h
+    })
 }
